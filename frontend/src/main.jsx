9a7386d81a314a087999efc1853f0d58c3fcab80
TimeFi Developer <dev@timefi.protocol>
2026-03-12 09:28:55 +0100

    Enable global toast notification provider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { AppRouter } from './Router';
 import { ThemeProvider } from './context/ThemeContext';
 import { WalletProvider } from './context/WalletContext';
+import { ToastProvider } from './components/Toast';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
       <WalletProvider>
-        <AppRouter />
+        <ToastProvider>
+          <AppRouter />
+        </ToastProvider>
       </WalletProvider>
     </ThemeProvider>
   </React.StrictMode>

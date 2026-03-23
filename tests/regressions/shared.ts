2997c300d442c7a417c4483e1cff36edfe0cb240
Adekunle Bamzz <bams.kunle@gmail.com>
2026-03-23 14:23:37 +0100

    test: add shared regression constants

---
+++ tests/regressions/shared.ts
@@ -0,0 +1,6 @@
+export const VAULT_CONTRACT_NAME = "timefi-vault";
+
+export const accounts = simnet.getAccounts();
+
+export const wallet1 = accounts.get("wallet_1")!;
+export const deployer = accounts.get("deployer")!;

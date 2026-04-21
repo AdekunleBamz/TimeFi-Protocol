871db1a85a43e9e3a1e79cd729b8f27bf2dbde57
AdekunleBamz <bams.kunle@gmail.com>
2026-04-21 14:29:56 +0100

    feat: add calcFee math utility

--- frontend/src/utils/math.js
+++ frontend/src/utils/math.js
@@ -2,3 +2,5 @@
 export const microStxToStx = (v) => Number(v) / 1e6;
 
 export const stxToMicroStx = (v) => Math.round(Number(v) * 1e6);
+
+export const calcFee = (amount, bps) => Math.floor(Number(amount) * Number(bps) / 10000);

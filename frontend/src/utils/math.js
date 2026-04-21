2dbea84628b86637ffe8e1310af3afa2541f8b75
AdekunleBamz <bams.kunle@gmail.com>
2026-04-21 14:29:57 +0100

    feat: add calcNetAmount math utility

--- frontend/src/utils/math.js
+++ frontend/src/utils/math.js
@@ -4,3 +4,5 @@ export const microStxToStx = (v) => Number(v) / 1e6;
 export const stxToMicroStx = (v) => Math.round(Number(v) * 1e6);
 
 export const calcFee = (amount, bps) => Math.floor(Number(amount) * Number(bps) / 10000);
+
+export const calcNetAmount = (amount, bps) => Number(amount) - Math.floor(Number(amount) * Number(bps) / 10000);

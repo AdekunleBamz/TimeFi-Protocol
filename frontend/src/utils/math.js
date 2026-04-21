fa95481d9efec6f6f1c0fc8a666cfdd6c633949f
AdekunleBamz <bams.kunle@gmail.com>
2026-04-21 14:29:59 +0100

    feat: add secondsToBlocks math utility

--- frontend/src/utils/math.js
+++ frontend/src/utils/math.js
@@ -8,3 +8,5 @@ export const calcFee = (amount, bps) => Math.floor(Number(amount) * Number(bps) 
 export const calcNetAmount = (amount, bps) => Number(amount) - Math.floor(Number(amount) * Number(bps) / 10000);
 
 export const blocksToSeconds = (blocks) => Number(blocks) * 600;
+
+export const secondsToBlocks = (secs) => Math.ceil(Number(secs) / 600);

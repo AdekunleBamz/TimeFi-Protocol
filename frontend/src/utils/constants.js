e30e94a132d72a450d54a2e2ca6580cf1c59fa8c
AdekunleBamz <bams.kunle@gmail.com>
2026-04-28 13:40:08 +0100

    Terminate blocks per hour export

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -268,7 +268,7 @@ export const MIN_DEPOSIT_MICRO_STX = 10000;
 
 export const STX_DECIMALS = 6;
 
-export const BLOCKS_PER_HOUR = 6
+export const BLOCKS_PER_HOUR = 6;
 
 export const BLOCKS_PER_DAY = 144
 

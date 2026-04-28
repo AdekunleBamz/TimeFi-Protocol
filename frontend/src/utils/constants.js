f58e22bf7027d7f5c56c672f519de4872bed7ef6
AdekunleBamz <bams.kunle@gmail.com>
2026-04-28 13:39:14 +0100

    Terminate minimum deposit export

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -264,7 +264,7 @@ export const MAX_LOCK_BLOCKS = 52560;
 
 export const FEE_BASIS_POINTS = 50;
 
-export const MIN_DEPOSIT_MICRO_STX = 10000
+export const MIN_DEPOSIT_MICRO_STX = 10000;
 
 export const STX_DECIMALS = 6
 
